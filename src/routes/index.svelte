76d8964179a0b76a8fad4ba9d8896e6f61279577
Claude Betz <claudebz@amazon.com>
2022-05-29 15:57:52 +0200

    Add tags

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,9 +2,31 @@
 	<title>Conduit</title>
 </svelte:head>
 
+<script context="module">
+    export async function load({ url, fetch }) {
+        const [{ articles, pages }, { tags }] = await Promise.all([
+            fetch(`/articles.json${url.search}`, { credentials: 'include' }).then((r) => r.json()),
+            fetch('/tags.json').then((r) => r.json())
+        ]);
+
+        return {
+            props: {
+                articles,
+                pages,
+                tags
+            }
+        };
+    }
+</script>
+
 <script>
     import { page } from '$app/stores';
+    $: tag = $page.url.searchParams.get('tag');
     $: tab = $page.url.searchParams.get('tab') || 'all';
+
+    export let articles;
+	export let pages;
+	export let tags;
 </script>
 
 
@@ -26,19 +48,31 @@
 								href="/?tab=all"
 								rel="prefetch"
 								class="nav-link"
-								class:active={tab === 'all'}
+								class:active={tab === 'all' && !tag}
 							>
 								Global Feed
 							</a>
 						</li>
 
                         <li class="nav-item">
-                            <a href="/?tab=feed" rel="prefetch" class="nav-link" class:active={tab === 'feed'}>
+                            <a
+                                href="/?tab=feed"
+                                rel="prefetch"
+                                class="nav-link"
+                                class:active={tab === 'feed'}
+                            >
                                 Your Feed
                             </a>
                         </li>
+
                         <li class="nav-item">
-                            <a href="/login" rel="prefetch" class="nav-link">Sign in to see your Feed </a>
+                            <a
+                                href="/login"
+                                rel="prefetch"
+                                class="nav-link"
+                            >
+                                Sign in to see your Feed
+                            </a>
                         </li>
 
                         <li class="nav-item">
@@ -52,6 +86,9 @@
 				<div class="sidebar">
 					<p>Popular Tags</p>
 					<div class="tag-list">
+						{#each tags as tag}
+							<a href="/?tag={tag}" rel="prefetch" class="tag-default tag-pill"> {tag} </a>
+						{/each}
 					</div>
 				</div>
 			</div>
